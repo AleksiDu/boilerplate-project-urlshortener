7d5874ecf8f58bff450f434b59261e1464bc5036
AleksiDu <aleksi.duluzauri@gmail.com>
2022-05-03 18:38:51 +0400

    add mongodb & mongoose

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+
+//Database Connection
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, {
+  userNewUrlParser: true,
+  userUndifiedTopology: true,
+  serverSelectionTimeoutMS: 10000 // Timeout afer 10 s instead of 30s
+});
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -19,6 +29,16 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// #3 URL Shortener Microservice
+// const Schema = mongoose.Schema;
+app.get('/api/shorturl', (req, res) => {
+  var original_url = req.originalUrl;
+  console.log(original_url);
+  res.json({ 
+    original_url: original_url }); 
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
