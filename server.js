6f09af5912666fc74dde9a67bfb920f7b74e3c77
AleksiDu <aleksi.duluzauri@gmail.com>
2022-05-06 13:59:45 +0400

    add suffix

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongo = require('mongodb');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const bodyParser = require('body-parser');
+const shortid = require('shortid');
 const app = express();
-const port = process.env.PORT || 3000;
-
-
-//Database Connection
-
-const uri = process.env.MONGO_URI;
-mongoose.connect(uri);
 
 // Basic Configuration
+const port = process.env.PORT || 3000;
+// Server conection 
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(cors());
 
@@ -28,69 +26,50 @@ app.get('/api/hello', function(req, res) {
 });
 
 // #3 URL Shortener Microservice
-const Schema = mongoose.Schema;
-const urlSchema = new Schema({
+// Mongoose Model & Schema
+const schema = new mongoose.Schema({ 
+  short_url: String,
   original_url: String,
-  short_url: String
+  suffix: String
 });
-const URL = mongoose.model("URL", urlSchema);
+const ShortURL = mongoose.model('ShortURL', schema);
 
-app.post('/api/shorturl/new',  async (req, res) => {
-  let url = req.body.url_input;
-  let urlCode = shortId.generate();
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-  //If url is valid
-  if (!validUrl.isWebUri(url)) {
-    res.status(401).json({
-      error: 'invalid URL'
+// parse application/json
+app.use(bodyParser.json())
+
+app.post('/api/shorturl/', (req, res) => {
+  let client_req_url = req.body.url;
+  let suffix = shortid.generate();
+  let newShortURL = suffix;
+
+  let newURL = new ShortURL({
+    short_url: __dirname + "/api/shorturl/" + suffix,
+    original_url: client_req_url,
+    suffix: suffix
+  })
+
+  newURL.save((err, doc) => {
+    if (err) return console.error(err);
+    res.json({
+      "saved": true,
+      "short_url": newURL.short_url,
+      "original_url": newURL.original_url,
+      "suffix": newURL.suffix
     });
-  } else {
-    try {
-      //If its already in the database
-      let findOne = await URL.findOne({
-        original_url: url
-      });
-      if (findOne) {
-        res.json({
-          original_url: findOne.original_url,
-          short_url: findOne.short_url
-        });
-      } else {
-        // if its not exist yet then create new one and response with the result
-        findOne = new URL({
-          original_url: url,
-          short_url: urlCode
-        });
-        await findOne.save()
-        res.json({
-          original_url: findOne.original_url,
-          short_url: findOne.short_url
-        });
-      };
-      } catch (err) {
-        console.error(err);
-        res.status(500).json('Server erorr...');
-    }
-  }
+  });
 });
 
-app.get('/api/shorturl/:short_url?', async (req,res) => {
-  try {
-    let urlParams = await URL.findOne({
-      short_url: req.params.short_url
-    });
-    if (urlParams) {
-      return res.redirect(urlParams.original_url);
-    } else {
-      return res.status(404).json('No URL found');
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json('Server error')
-  }
+app.get('/api/shorturl/:suffix', (req, res) => {
+  let userGeneratedSuffix = req.params.suffix;
+  ShortURL.find({suffix: userGeneratedSuffix}).then(foundUrls => {
+  let urlForRederect = foundUrls[0];
+  res.redirect(urlForRederect.original_url);
+  });
 });
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
-
