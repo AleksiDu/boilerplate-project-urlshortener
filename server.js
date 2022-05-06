7bf9e8b9536208ea5fbe161d0ebea24e2bd54e74
AleksiDu <aleksi.duluzauri@gmail.com>
2022-05-06 19:00:16 +0400

    add dns lookup & url valid

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongo = require('mongodb');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const shortid = require('shortid');
+const lookup = require('dns-lookup');
+const validUrl = require("valid-url");
 const app = express();
 
 // Basic Configuration
@@ -16,18 +18,19 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
 // #3 URL Shortener Microservice
 // Mongoose Model & Schema
-const schema = new mongoose.Schema({ 
+const schema = new mongoose.Schema({
   short_url: String,
   original_url: String,
   suffix: String
@@ -41,35 +44,37 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.post('/api/shorturl/', (req, res) => {
-  let client_req_url = req.body.url;
-  let suffix = shortid.generate();
-  let newShortURL = suffix;
-
-  let newURL = new ShortURL({
-    short_url: __dirname + "/api/shorturl/" + suffix,
-    original_url: client_req_url,
-    suffix: suffix
-  })
-
-  newURL.save((err, doc) => {
-    if (err) return console.error(err);
-    res.json({
-      "saved": true,
-      "short_url": newURL.short_url,
-      "original_url": newURL.original_url,
-      "suffix": newURL.suffix
+  let client_req_url = validUrl.isWebUri(req.body.url);
+  if (client_req_url != undefined) {
+    let suffix = shortid.generate();
+    let newShortURL = suffix;
+    let newURL = new ShortURL({
+      short_url: __dirname + "/api/shorturl/" + newShortURL,
+      original_url: client_req_url,
+      suffix: suffix
     });
+    newURL.save((err, doc) => {
+      if (err) return console.error(err);
+      res.json({
+        "saved": true,
+        "short_url": newURL.short_url,
+        "original_url": newURL.original_url,
+        "suffix": newURL.suffix
+      });
+    });
+  } else {
+    res.json({"error":"invalid URL"});
+  }
+});
+
+app.get('/api/shorturl/:short', (req, res) => {
+  let userGeneratedSuffix = req.params.short;
+  ShortURL.find({ short: userGeneratedSuffix }).then((foundUrls) => {
+    let urlForRederect = foundUrls[0];
+    res.redirect(urlForRederect.original_url);
   });
 });
 
-app.get('/api/shorturl/:suffix', (req, res) => {
-  let userGeneratedSuffix = req.params.suffix;
-  ShortURL.find({suffix: userGeneratedSuffix}).then(foundUrls => {
-  let urlForRederect = foundUrls[0];
-  res.redirect(urlForRederect.original_url);
-  });
-});
-
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
